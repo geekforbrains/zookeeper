f1bf187f5dc0f0a1d2d90d769440b7d3ffcc4863
Gavin Vickery <gavin@geekforbrains.com>
2014-04-21 20:05:51 -0700

    Converting wrapped functions to classes. Updated tests.

--- test/all.js
+++ test/all.js
@@ -1,19 +1,22 @@
 var url = require('url');
 var prompt = require('prompt');
 var expect = require('chai').expect;
-var zookeeper = require('../lib/zookeeper');
+var Zookeeper = require('../lib/zookeeper').Zookeeper;
 var config = require('./config');
 
 // Set during tests
 var OAUTH_TOKEN = null;
 var OAUTH_TOKEN_SECRET = null;
-var TOKEN = null;
+var TOKEN = '';
 
 describe('auth.start', function() {
   it('should return oauth tokens and authorize url', function(done) {
-    var zk = zookeeper({consumerKey: config.consumerKey, consumerSecret: config.consumerSecret});
+    var zk = new Zookeeper({
+      consumerKey: config.consumerKey,
+      consumerSecret: config.consumerSecret
+    });
 
-    zk.auth.start('http://localhost', function(err, authData) {
+    zk.auth().start('http://localhost', function(err, authData) {
       expect(authData).to.have.property('oauthToken');
       expect(authData).to.have.property('oauthTokenSecret');
       expect(authData).to.have.property('authorizeUrl');
@@ -28,7 +31,10 @@ describe('auth.start', function() {
 describe('auth.finish', function() {
   it('should return access token and secret', function(done) {
     this.timeout(0); // Disable Mocha timeout so prompt can wait for user input
-    var zk = zookeeper({consumerKey: config.consumerKey, consumerSecret: config.consumerSecret});
+    var zk = new Zookeeper({
+      consumerKey: config.consumerKey,
+      consumerSecret: config.consumerSecret
+    });
 
     prompt.start();
     prompt.get('callbackUrl', function(err, promptData) {
@@ -37,6 +43,7 @@ describe('auth.finish', function() {
         expect(authData).to.have.property('token');
         expect(authData).to.have.property('tokenSecret');
         TOKEN = authData.token;
+        console.log('Access Token: ' + TOKEN);
         done();
       });
     });
@@ -45,9 +52,9 @@ describe('auth.finish', function() {
 
 describe('user.info', function() {
   it('should return an evernote user object', function(done) {
-    var zk = zookeeper({token: TOKEN});
+    var zk = new Zookeeper({token: TOKEN});
 
-    zk.user.info(function(err, userData) {
+    zk.user().info(function(err, userData) {
       expect(userData).to.have.property('id');
       expect(userData).to.have.property('username');
       done();
@@ -56,11 +63,28 @@ describe('user.info', function() {
 });
 
 describe('user.notebooks', function() {
-  it('should return an array of notebooks', function(done) {
+  it('should return an array of zookeeper notebook objects', function(done) {
+    var zk = new Zookeeper({token: TOKEN});
+
+    zk.user().notebooks(function(err, notebooks) {
+      expect(notebooks instanceof Array).to.equal(true);
+      expect(notebooks[0] instanceof Zookeeper.Notebook).to.equal(true);
+      done();
+    });
+  });
+});
+
+describe('user.notes', function() {
+  it('should return an array of note objects', function(done) {
     var zk = zookeeper({token: TOKEN});
 
-    zk.user.notebooks(function(err, notebooks) {
-      // TODO
+    zk.user.notes(function(err, notes) {
+      expect(notes instanceof Array).to.equal(true);
+      expect(notes.length > 0).to.equal(true);
+      notes[0].info(function(noteInfo) {
+        expect(noteInfo).to.have.property('guid');
+        done();
+      })
     });
   });
 });
