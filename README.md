bd0eee764390b09f996b90622ce5e7ebb01d840a
Gavin Vickery <gavin@geekforbrains.com>
2014-07-08 13:52:40 -0700

    Refactoring to use modules instead of classes. Updating tests.

--- README.md
+++ README.md
@@ -5,23 +5,36 @@ A tool for taming the Evernote API
 
 ```
 var zookeeper = require('zookeeper');
-var zk = zookeeper({token: 'your-access-token'});
+var zk = zookeeper({token: 'users-access-token'});
 
-zk.user.info(function(err, userData) {
-  console.log(userData);
+zk.user.info(function(err, info) {
+  // work with user info
 });
 
-zk.user.notebooks(function(err, notebooks) {
-  async.each(notebooks,
-    function(notebook, callback) {
-      notebook.notes(function(err, notes) {
-        console.log(notes);
-        callback(err);
-      });
-    },
-    function(err) {
-      console.log('Yay!');
-    }
-  );
+zk.notebooks.all(function(err, notebooks) {
+  // work with notebooks
 });
-```
+
+zk.notebooks.single(guid, options, function(err, notebook) {
+  // work with single notebook instance
+});
+
+zk.notes.all(function(err, notes) {
+  // work with all notes
+});
+
+zk.notes.single(guid, options, function(err, note) {
+  // work with single note instance
+});
+
+zk.notes.inNotebook(notebookGuid, function(err, notes) {  
+  // work with notes in specific notebook
+});
+
+zk.notes.taggedWith(tags, function(err, notes) {
+  // work with tagged notes
+});
+
+zk.notes.toHtml(guid, options, function(err, html) {
+  // single note converted into html
+});
--- test/all.js
+++ test/all.js
@@ -1,7 +1,7 @@
 var url = require('url');
 var prompt = require('prompt');
 var expect = require('chai').expect;
-var Zookeeper = require('../lib/zookeeper').Zookeeper;
+var zookeeper = require('../lib/zookeeper');
 var config = require('./config');
 
 // Set during tests
@@ -11,12 +11,12 @@ var TOKEN = '';
 
 describe('auth.start', function() {
   it('should return oauth tokens and authorize url', function(done) {
-    var zk = new Zookeeper({
+    var zk = zookeeper({
       consumerKey: config.consumerKey,
       consumerSecret: config.consumerSecret
     });
 
-    zk.auth().start('http://localhost', function(err, authData) {
+    zk.auth.start('http://localhost', function(err, authData) {
       expect(authData).to.have.property('oauthToken');
       expect(authData).to.have.property('oauthTokenSecret');
       expect(authData).to.have.property('authorizeUrl');
@@ -31,7 +31,8 @@ describe('auth.start', function() {
 describe('auth.finish', function() {
   it('should return access token and secret', function(done) {
     this.timeout(0); // Disable Mocha timeout so prompt can wait for user input
-    var zk = new Zookeeper({
+
+    var zk = zookeeper({
       consumerKey: config.consumerKey,
       consumerSecret: config.consumerSecret
     });
@@ -52,11 +53,11 @@ describe('auth.finish', function() {
 
 describe('user.info', function() {
   it('should return an evernote user object', function(done) {
-    var zk = new Zookeeper({token: TOKEN});
+    var zk = zookeeper({token: TOKEN});
 
-    zk.user().info(function(err, userData) {
-      expect(userData).to.have.property('id');
-      expect(userData).to.have.property('username');
+    zk.user.info(function(err, info) {
+      expect(info).to.have.property('id');
+      expect(info).to.have.property('username');
       done();
     });
   });
@@ -64,11 +65,10 @@ describe('user.info', function() {
 
 describe('user.notebooks', function() {
   it('should return an array of zookeeper notebook objects', function(done) {
-    var zk = new Zookeeper({token: TOKEN});
+    var zk = zookeeper({token: TOKEN});
 
-    zk.user().notebooks(function(err, notebooks) {
-      expect(notebooks instanceof Array).to.equal(true);
-      expect(notebooks[0] instanceof Zookeeper.Notebook).to.equal(true);
+    zk.notebooks.all(function(err, notebooks) {
+      expect(notebooks[0]).to.have.property('guid');
       done();
     });
   });
@@ -78,13 +78,9 @@ describe('user.notes', function() {
   it('should return an array of note objects', function(done) {
     var zk = zookeeper({token: TOKEN});
 
-    zk.user.notes(function(err, notes) {
-      expect(notes instanceof Array).to.equal(true);
-      expect(notes.length > 0).to.equal(true);
-      notes[0].info(function(noteInfo) {
-        expect(noteInfo).to.have.property('guid');
-        done();
-      })
+    zk.notes.all(function(err, notes) {
+      console.log(notes);
+      done();
     });
   });
 });
